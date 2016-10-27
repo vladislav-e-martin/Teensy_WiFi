9e006bb1b95c2b166a04b02106b82618ab2ab29c
Vladislav Martin <vladislav.e.martin@gmail.com>
2016-10-27 17:32:25 -0400

    Updated Testing Code: Teensy_ReadTweet Compiles
    Teensy_ReadTweet now successfully compiles, so it is assumed that
    whatever is typed into the serial monitor (directly from the computer
    connected to the Teensyduino) will be communicated to the HUZZAH board
    over an I2C connection. This has still not been tested, so it is not
    certain that this code is production-ready.

--- Huzzah_SendTweet/Huzzah_SendTweet.ino
+++ Huzzah_SendTweet/Huzzah_SendTweet.ino
@@ -1,10 +1,10 @@
 #include <ESP8266WiFi.h>
  
-const char* ssid     = "yourssid";
-const char* password = "yourpassword";
- 
-const char* host = "wifitest.adafruit.com";
- 
+const char* ssid     = "yourssid"; // Name of WiFi network (i.e., NUwave)
+unsigned char identity[] = "universityusername"; // Username for the chosen WPA-2 Enterprise WiFi network (i.e., NU username)
+const char* password = "yourpassword"; // Password for the chosen WPA-2 Enterprise WiFi network (i.e., NU password)
+const char* host     = "wifitest.adafruit.com"; // Tweeting Host
+
 void setup() 
 {
   Serial.begin(115200);
@@ -13,13 +13,18 @@ void setup()
   // We start by connecting to a WiFi network
  
   Serial.println();
-  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  wifi.station_set_username(identity, sizeof(identity));
+
+  // Maybe do something with certification key -- this must be included manually, though
   
   WiFi.begin(ssid, password);
   
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) 
+  {
     delay(500);
     Serial.print(".");
   }
@@ -43,7 +48,8 @@ void loop()
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
   const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
+  if (!client.connect(host, httpPort)) 
+  {
     Serial.println("connection failed");
     return;
   }
@@ -54,13 +60,15 @@ void loop()
   Serial.println(url);
   
   // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
+  client.print(String("GET ") + 
+    url + " HTTP/1.1\r\n" +
+    "Host: " + host + "\r\n" + 
+    "Connection: close\r\n\r\n");
   delay(500);
   
   // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
+  while(client.available())
+  {
     String line = client.readStringUntil('\r');
     Serial.print(line);
   }
--- Teensy_ReadTweet/Teensy_ReadTweet.ino
+++ Teensy_ReadTweet/Teensy_ReadTweet.ino
@@ -1,37 +1,23 @@
 /*
-  Serial Event example
-
+ * 
+ * 
  When new serial data arrives, this sketch adds it to a String.
  When a newline is received, the loop prints the string and
  clears it.
-
- A good test for this is to try it with a GPS receiver
- that sends out NMEA 0183 sentences.
-
- Created 9 May 2011
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/SerialEvent
-
+ *
+ *
  */
  
 #include <Wire.h>
-
-char* inputString = ""; // A string to hold incoming data
-boolean stringComplete = false;  // Whether the string is complete
 
 void setup() 
 {
   // Initialize Serial connection
   Serial.begin(9600);
-  // Reserve 1000 bytes (250 characters) for the inputString
-  // inputString.reserve(1000);
-
-  // Code to receive the message from the Tx node
-  // If a message is received, store it in inputString and set stringComplete = true
-  // Otherwise, stringComplete = false
+  while (!Serial)
+  {
+    ; // Wait for the serial port to connect.
+  }
 
   // Join the I2C bus between microcontrollers
   Wire.begin(); // Address is optional for master
@@ -39,16 +25,20 @@ void setup()
 
 void loop() 
 {
-  // print the string when a newline arrives:
-  if (stringComplete)
-  {
-    Serial.println(inputString);
+  String inputString; // String to hold incoming data
+  char inChar; // Char to hold each incoming character
+  boolean stringComplete = false; // Tell if the incoming string is complete
+  
+  // If the Teensy receives a message over Serial connection, store it in inputString
+  inputString = readStringFromSerial();
 
-    Wire.beginTransmission(8); // Transmit the message to the device at address #8
-    Wire.write("The message is: "); // Send 16 bytes 
-    Wire.write(inputString); // Send 1000 bytes
+  if (inputString.length() != 0)
+  {
+    Serial.println("The message is: " + inputString);
+    Wire.beginTransmission(8); // Transmit the message to the device at address #8 
+    Wire.write(inputString.c_str()); // Sends the input string to the 
     Wire.endTransmission(); // Stop transmitting the message
-    
+      
     // Clear the contents of the inputString
     inputString = "";
     // Reset the Serial.read() completion indicator, stringComplete
@@ -66,20 +56,13 @@ void loop()
  response.  Multiple bytes of data may be available.
 */
 
-void serialEvent() 
+String readStringFromSerial() 
 {
+  String stringFromSerial;
   // Check if the Serial connection has been established
-  while (Serial.available()) 
+  while (Serial.available() > 0) 
   {
-    // Read the newest byte
-    char inChar = (char)Serial.read();
-    // Add the newest byte to the inputString
-    inputString += inChar;
-    // If the newest byte is a newline, then the user has finished typing the inputString
-    if (inChar == '\n')
-    {
-      // Indicate that the user has finished typing the inputString
-      stringComplete = true;
-    }
+    stringFromSerial = Serial.readStringUntil('\n');
+    return stringFromSerial;
   }
 }
